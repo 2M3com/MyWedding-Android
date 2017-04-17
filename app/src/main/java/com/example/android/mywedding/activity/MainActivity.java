69b465a02b855c09bb01cf9ac210a6abf90f264a
Aaron Socas <aaron.socas@atos.net>
2017-04-17 14:58:26 +0100

    added dev and pro envirnment

--- app/src/main/java/com/example/android/mywedding/activity/MainActivity.java
+++ app/src/main/java/com/example/android/mywedding/activity/MainActivity.java
@@ -1,12 +1,18 @@
-package com.example.android.mywedding;
+package com.example.android.mywedding.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.android.mywedding.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "STEP";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, this.getClass().toString() + "::onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
---
+++ app/src/pro/java/com/example/android/mywedding/Enviroment.java
@@ -0,0 +1,8 @@
+package com.example.android.mywedding;
+
+
+public final class Enviroment {
+
+    public static final String ENVIROMENT = "PRO";
+
+}
