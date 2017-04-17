a6f0d18cf7bfa37fee3439ecc948b40bbb2f2dfd
Aaron Socas <aaron.socas@atos.net>
2017-04-17 15:03:04 +0100

    enviroments checked

--- app/src/main/java/com/example/android/mywedding/activity/MainActivity.java
+++ app/src/main/java/com/example/android/mywedding/activity/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.android.mywedding.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.android.mywedding.Enviroment;
 import com.example.android.mywedding.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, this.getClass().toString() + "::onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView hello = (TextView)findViewById(R.id.hello);
+        if(Enviroment.ENVIROMENT.equals("DEV") ){
+            hello.setText("Hello DEV");
+        } else {
+            hello.setText("Hello PRO");
+        }
     }
 }
